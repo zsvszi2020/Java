d51be4564e0a5ee4cec9a2f360666b71253ec849
unipen_1_1.pyunipen2022 <unipen_1_1.pyunipen2022@gmail.com>
2023-07-17 17:53:12 +0200

    2. commit

--- Week_1/Main.java
+++ Week_1/Main.java
@@ -1,9 +1,28 @@
 package Week_1 ;
+import java.util.* ;
 
 public class Main 
 {
+   public static final int n = 5 ;
+   public static int matrix[][] ;
    public static void main(String[] args)
    {
-      System.out.println("Hello Word") ;
+      matrix = new int[n][n] ;
+      Random rand = new Random() ;
+      for (int i = 0 ; i < n ; i++)
+      {
+         for (int j = 0 ; j  < n ; j++)
+         {
+            matrix[i][j] = Math.abs(rand.nextInt()) % 8  ;
+         }
+      }
+      for (int i = 0 ; i < n ; i++)
+      {
+         for (int j = 0 ; j  < n ; j++)
+         {
+            System.out.print(matrix[i][j]+ " ");
+         }
+         System.out.println();
+      }
    }
 }
